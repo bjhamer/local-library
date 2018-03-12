54b10e4581d4049bb8e1147bd778ced16068cb6e
Brad <bhamer477@gmail.com>
2018-03-12 12:37:25 -0700

    Created User Authentication with login and logout functionality. Admins have the ability to loan books through admin site. Users can now be created and view what books they have borrowed and when the due date is.

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 
@@ -17,6 +18,10 @@ def index(request):
     num_authors=Author.objects.count() # The 'all()' is implied by default
     num_genres=Genre.objects.count()
 
+    # Number of visits to this view, as counted in the session variable
+    num_visits=request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits+1
+
     # Render the HTML template index.html with the data in the context variable
     return render(
         request,
@@ -27,6 +32,7 @@ def index(request):
             'num_instances_available': num_instances_available,
             'num_authors': num_authors,
             'num_genres': num_genres,
+            'num_visits': num_visits
         },
     )
 
@@ -45,3 +51,11 @@ class AuthorListView(generic.ListView):
 
 class AuthorDetailView(generic.DetailView):
     model = Author
+
+class LoanedBooksByUserListView(LoginRequiredMixin, generic.ListView):
+    model = BookInstance
+    template_name = 'catalog/bookinstance_list_borrowed_user.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        return BookInstance.objects.filter(borrower=self.request.user).filter(status__exact='o').order_by('due_back')
